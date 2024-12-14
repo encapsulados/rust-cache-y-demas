3519ee9028e479be553e88c438dadb0b1ecb9156
Andrés González <a.gonzalez.terres@gmail.com>
2024-12-14 16:49:07 -0300

    http

--- src/http.rs
+++ src/http.rs
@@ -19,7 +19,10 @@ mod servicio_de_datos {
     // dado "foo" -> devolver "bar"
     // otra cosa  -> devolver un error
     pub async fn traer_datos(palabra_clave: String) -> Result<String, CódigoDeErrorPersonalizado> {
-        todo!()
+        match palabra_clave.as_str() {
+            "foo" => Ok("bar".to_string()),
+            _ => Err(CódigoDeErrorPersonalizado::nuevo(palabra_clave))
+        }
     }
 }
 
@@ -27,7 +30,10 @@ mod servicio_http {
     use crate::http::servicio_de_datos;
 
     pub async fn procesar_consulta(palabra_clave: String) -> Result<i32, i32> {
-        todo!()
+        servicio_de_datos::traer_datos(palabra_clave)
+            .await
+            .map(|_| 200)
+            .map_err(|_| 500)
     }
 }
 

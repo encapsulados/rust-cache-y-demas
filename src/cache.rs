bf11b19f1aa68ae5aacc2ae327f999893bc33cb1
Andrés González <a.gonzalez.terres@gmail.com>
2024-12-16 10:47:56 -0300

    lista enlazada sacar nodo arbitrario

--- src/cache.rs
+++ src/cache.rs
@@ -18,3 +18,49 @@
 // obtener("cuba")   -> -1 // La clave "cuba" fue eliminada porque la caché estaba llena.
 // obtener("ramona") -> 4  // La clave "ramona" sigue en la caché.
 // obtener("nube")   -> 9  // La clave "nube" sigue en la caché.
+
+use std::cell::RefCell;
+use std::rc::Rc;
+
+type NodoRef = Rc<RefCell<Nodo>>;
+
+struct Nodo {
+    elemento: i32,
+    anterior: Option<NodoRef>,
+    siguiente: Option<NodoRef>,
+}
+
+struct Lista {
+    cabeza: Option<NodoRef>,
+    cola: Option<NodoRef>,
+}
+
+impl Lista {
+    fn agregar_cabeza(&self, nodo: NodoRef) {
+        todo!()
+    }
+
+    fn sacar_cola(&self) -> NodoRef {
+        todo!()
+    }
+
+    fn sacar(&self, nodo: NodoRef) -> NodoRef {
+        // si el nodo es la cabeza
+        // si el nodo es la cola
+
+        // si el nodo esta en el medio
+        // al anterior apuntarle el siguiente
+        // al siguiente apuntarle el anterior
+        // al nodo le pongo nada como anterior y siguiente
+        let anterior = nodo.borrow().anterior.clone();
+        let siguiente = nodo.borrow().siguiente.clone();
+
+        anterior.clone().map(|anterior| anterior.borrow_mut().siguiente = siguiente.clone());
+        siguiente.map(|siguiente| siguiente.borrow_mut().anterior = anterior);
+
+        nodo.borrow_mut().anterior = None;
+        nodo.borrow_mut().siguiente = None;
+        nodo
+    }
+
+}
